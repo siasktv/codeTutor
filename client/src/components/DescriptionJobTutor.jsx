a19e7d8c3980bb415aa4247dc33a17712eeb41a5
Lautaro Barrios <barrios.g.lautaro@gmail.com>
2023-06-14 19:24:33 -0300

    Modularizado el Tutor y sus componentes--Cards

--- client/src/components/DescriptionJobTutor.jsx
+++ client/src/components/DescriptionJobTutor.jsx
@@ -1,7 +1,7 @@
 const DescriptionJobTutor = () => {
     return (
         <h2 className="font-semibold text-sm text-left">
-            Trabaje en el mundo del espectaculo para dedicarme a mi otra pasión...el skate. Ah y también a programar. Mollit in laborum tempor Lorem incididunt irure. Aute eu ex ad sunt. Pariatur sint culpa do incididunt eiusmod eiusmod culpa. laborum tempor Lorem incididunt.
+            Trabaje en el mundo del espectáculo para dedicarme a mi otra pasión...el skate. Ah y también a programar. Mollit in laborum tempor Lorem incididunt irure. Aute eu ex ad sunt. Pariatur sint culpa do incididunt eiusmod eiusmod culpa. laborum tempor Lorem incididunt.
         </h2>
     )
 }
