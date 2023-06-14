89acaea5f0bafce6ea1b163f6ac35ba15aad832c
Dante De Nicoló <denicolodante@gmail.com>
2023-06-14 17:07:59 -0300

    login / signup model

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,19 @@
-
 import './App.css'
-import { Routes,Route,Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Landing from './views/Landing'
-import SearchPage  from './views/SearchPage'
+import SearchPage from './views/SearchPage'
 import TutorProfile from './views/TutorProfile'
+import TestLoginPage from './views/testLoginPage'
 
-function App() {
-
+function App () {
   return (
     <div className='App'>
       <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path='/home' element={<Landing/>} />
-      <Route path='/search' element={<SearchPage/>} />
-      <Route path='/tutor' element={<TutorProfile/>} />
-
-
-
+        <Route path='/' element={<Navigate to='/home' />} />
+        <Route path='/home' element={<Landing />} />
+        <Route path='/search' element={<SearchPage />} />
+        <Route path='/tutor' element={<TutorProfile />} />
+        <Route path='/testlogin' element={<TestLoginPage />} />
       </Routes>
     </div>
   )
