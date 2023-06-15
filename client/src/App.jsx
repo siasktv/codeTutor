361cef5d7c0c85c9a7f5917ba8546d1007474eb5
Dante De Nicoló <denicolodante@gmail.com>
2023-06-15 20:50:08 -0300

    Login y register funcional

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,28 @@
 import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
-
 import { NavUserNotifications } from './components'
-import { SearchPage, TutorProfile, TestLoginPage, Landing,Login,Register } from './views'
+import {
+  SearchPage,
+  TutorProfile,
+  // TestLoginPage,
+  Landing,
+  Login,
+  Register
+} from './views'
 
 function App () {
   return (
     <div className='App'>
       <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path='/home' element={<Landing/>} />
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/search' element={<SearchPage/>} />
-      <Route path='/tutor' element={<TutorProfile/>} />
-      <Route path='/testlogin' element={<TestLoginPage />} />
-      <Route path='/navuser' element={<NavUserNotifications />} />
-
+        <Route path='/' element={<Navigate to='/home' />} />
+        <Route path='/home' element={<Landing />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/search' element={<SearchPage />} />
+        <Route path='/tutor' element={<TutorProfile />} />
+        {/* <Route path='/testlogin' element={<TestLoginPage />} /> */}
+        <Route path='/navuser' element={<NavUserNotifications />} />
       </Routes>
     </div>
   )
