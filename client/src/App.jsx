02cce00761706f9857daf2b4fd9147554919601d
siasktv <biankmbm92@gmail.com>
2023-06-15 21:06:34 -0400

    Form/Tutor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,30 @@
 import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
-<<<<<<< HEAD
-import { NavLogin } from './components'
+
+import { NavUserNotifications } from './components'
 import {
   SearchPage,
   TutorProfile,
   TestLoginPage,
   Landing,
+  Login,
+  Register,
   TutorForm,
 } from './views'
-=======
-
-
-import { NavUserNotifications } from './components'
-import { SearchPage, TutorProfile, TestLoginPage, Landing,Login,Register } from './views'
->>>>>>> 4b87be0e9202a319872179430143347d26ec5087
 
 function App() {
   return (
     <div className="App">
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Landing />} />
-        <Route path="/nav" element={<NavLogin />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/tutor" element={<TutorProfile />} />
         <Route path="/testlogin" element={<TestLoginPage />} />
+        <Route path="/navuser" element={<NavUserNotifications />} />
         <Route path="/form" element={<TutorForm />} />
-=======
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path='/home' element={<Landing/>} />
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/search' element={<SearchPage/>} />
-      <Route path='/tutor' element={<TutorProfile/>} />
-      <Route path='/testlogin' element={<TestLoginPage />} />
-      <Route path='/navuser' element={<NavUserNotifications />} />
-
->>>>>>> 4b87be0e9202a319872179430143347d26ec5087
       </Routes>
     </div>
   )
--- client/src/views/TutorForm.jsx
+++ client/src/views/TutorForm.jsx
@@ -1,5 +1,16 @@
+import { NavUserNotifications } from '../components'
+
 const TutorForm = () => {
-  return <></>
+  return (
+    <div>
+      <NavUserNotifications />
+      <section className="bg-[#D9D9D9] -mx-[80px] w-screen h-screen ">
+        <h2 className="text-[#05004E] text-[30px]">
+          ¡Hola, Shi Juan! ¡Bienvenido!
+        </h2>
+      </section>
+    </div>
+  )
 }
 
 export default TutorForm
