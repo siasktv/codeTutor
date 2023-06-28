64cc855acc878ff01cb152c81a0bbd602ce8f83e
siasktv <biankmbm92@gmail.com>
2023-06-28 09:51:37 -0400

    Stripe

--- server/Handlers/Payments/paymentsHandler.js
+++ server/Handlers/Payments/paymentsHandler.js
@@ -1,31 +1,35 @@
 require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+const FRONTEND_URL = process.env.FRONTEND_URL
 const Session = require('../../models/Session.models')
 
 const createPaymentIntent = async (req, res) => {
   try {
-    const { priceId } = req.body
+    const paymentDetails = req.body
+
+    console.log(paymentDetails)
 
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
           price_data: {
             currency: 'usd',
-            unit_amount: 2000,
+            unit_amount: paymentDetails.amount * 100,
             product_data: {
-              name: 'T-shirt',
-              description: 'Comfortable cotton t-shirt',
-              images: ['https://example.com/t-shirt.png'],
+              name: 'Session',
+              description: paymentDetails.description,
             },
           },
           quantity: 1,
         },
       ],
       mode: 'payment',
-      success_url: 'http://localhost:5173',
-      cancel_url: 'http://localhost:5173',
+      success_url: `${FRONTEND_URL}/meeting/${paymentDetails.sessionId}`,
+      //pasarle
+      cancel_url: `${FRONTEND_URL}`,
     })
 
+    console.log(session)
     res.json({ url: session.url })
   } catch (err) {
     res.status(500).json({ message: err.message })
