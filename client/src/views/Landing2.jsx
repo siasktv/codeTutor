1b4bef8266fd0c54fbf151cf878214d240585edc
Dante <denicolodante@gmail.com>
2023-07-10 16:16:49 -0300

    dark mode and responsive new landing

--- client/src/views/Landing2.jsx
+++ client/src/views/Landing2.jsx
@@ -4,16 +4,34 @@ import {
   Team,
   Testimonials,
   Footer,
-  NavLogin,
+  NavLogin
 } from '../components'
 
 import useUser from '../hooks/useUser'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { tutorsFetch } from '../redux/features/tutors/tutorsSlice'
 
 const Landing2 = () => {
+  const dispatch = useDispatch()
+  const tutors = useSelector(state => state.tutors.tutors)
+
   const user = useUser()
+
+  useEffect(() => {
+    if (!tutors[0]?.bio?.specialty) {
+      dispatch(tutorsFetch())
+    }
+  }, [dispatch])
+
+  useEffect(() => {
+    //scroll to top on route change
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
-    <section className="w-full min-h-screen bg-gray-50">
-      <div className="sticky top-0 z-[99999]">
+    <section className='w-full min-h-screen'>
+      <div className='sticky top-0 z-[99999]'>
         <NavLogin user={user} />
       </div>
       <Hero />
